03857804a4814dec0e57dd240ef0a5bb65c95c33
manasakoduri <81333454+manasakoduri@users.noreply.github.com>
2023-02-25 22:43:08 -0600

    1 file modified, 1 added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Nav from './Nav';
+import Intro from './Intro';
+import Blog from './Blog';
+import TechStack from './TechStack';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Intro />
+      <Blog />
+      <TechStack />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import Nav from './Nav';
-import Intro from './Intro';
-import Blog from './Blog';
-import TechStack from './TechStack';
+import App from './App'
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Nav />
-    <Intro />
-    <Blog />
-    <TechStack />
+    <App />
   </StrictMode>
 );
